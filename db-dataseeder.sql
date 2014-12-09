4d70c53172768efb0c8c9d2d4aa3c6f12dcc1460
Daniel Achleitner <daniel.achleitner@gmail.com>
2014-12-09 01:04:23 +0100

    Tables for users and pages

--- db-dataseeder.sql
+++ db-dataseeder.sql
@@ -1,2 +1,7 @@
 -- Setup wae07_* test data
 -- (don't modify the schema)
+
+INSERT INTO `wae07_pages` (`id`, `parent_id`, `title`, `content`)
+VALUES
+  (1, NULL, 'Welcome', 'Hi there!'),
+  (2, 1, 'Subpage', 'Hi there! This page has a parent.');
--- db-schema.sql
+++ db-schema.sql
@@ -1,2 +1,30 @@
 -- Setup wae07_* tables
 -- (don't drop any tables from other groups)
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS `wae07_users`;
+DROP TABLE IF EXISTS `wae07_pages`;
+
+CREATE TABLE `wae07_users` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL UNIQUE KEY,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `wae07_pages` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `title` TINYTEXT NOT NULL,
+  `content` TEXT NOT NULL,
+  `author_user_id` int NULL,
+  CONSTRAINT FOREIGN KEY (`parent_id`)
+    REFERENCES `wae07_pages` (`id`)
+    ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT FOREIGN KEY (`author_user_id`)
+    REFERENCES `wae07_users` (`id`)
+    ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
